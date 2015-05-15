dfef00768f8df5108d339436dd5eb27048861d65
David Begin <davidmichaelbe@gmail.com>
2015-05-14 20:13:54 -0700

    smallest addition to make the rust book compile

--- src/learn_rust_ownership.rs
+++ src/learn_rust_ownership.rs
@@ -1,3 +1,5 @@
+extern crate type_printer;
+
 // http://chrismorgan.info/blog/rust-ownership-the-hard-way.html
 
 pub fn the_hard_way() {
@@ -89,8 +91,31 @@ fn option_sidebar() {
     // So that means I can make a function that returns
     // either Some or None
 
-    println!("{:?}", do_not_get_greedy(5));
-    println!("{:?}", do_not_get_greedy(14));
+    // println!("{:?}", do_not_get_greedy(5));
+    // println!("{:?}", do_not_get_greedy(14));
+
+    // we can also now make functions that take an option
+    // meaning they can take either Some or None
+    // you_got_to_be_flexible(None);
+    // you_got_to_be_flexible(Some(10));
+
+    let this_thang: i32 = 11;
+
+    let num: Option<i32> = match this_thang {
+        10 => Some(10),
+        _ => None
+    };
+
+    // you_got_to_be_flexible(num)
+    if_let_example();
+}
+
+fn you_got_to_be_flexible(num: Option<i32>) {
+    if num.is_some() {
+        println!("You got to be flexible!");
+    } else {
+        println!("I got you covered");
+    }
 }
 
 fn do_not_get_greedy(num: i32) -> Option<i32> {
@@ -99,4 +124,17 @@ fn do_not_get_greedy(num: i32) -> Option<i32> {
     } else {
       None
     }
+}
+
+fn if_let_example() {
+    let option = Some(10);
+
+    match option {
+        Some(x) => { foo(x) },
+        None    => {        }
+    }
+}
+
+fn foo<T>(t: T) {
+    println!("I can do anything I put my mind to!");
 }
