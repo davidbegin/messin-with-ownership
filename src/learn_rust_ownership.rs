c1e4c3a84e8ea04a6a98e977bc367fe3eccdc258
David Begin <davidmichaelbe@gmail.com>
2015-05-14 20:51:01 -0700

    3 is natural as a rubyist
      one day this is what learning a new langauge, will be like
      for every feature!!!

--- src/learn_rust_ownership.rs
+++ src/learn_rust_ownership.rs
@@ -76,5 +76,19 @@ fn rule_2() {
 }
 
 fn rule_3() {
+    let rule_3: &str = "
+        Blocks produce a value which goes up one level of scope
+    ";
 
+    // YESSSS, finally something that makes sense!
+
+    // I mean
+    let x = {{{{{Some(1)}}}}};
+
+    // and I got a nice explicit return a semi-colon offical!
+    foo_the_c_way();
+}
+
+fn foo_the_c_way() -> i32 {
+    return 1;
 }
