b6cad839160256571ff9fe71f5ff5b91a3f217ec
David Begin <davidmichaelbe@gmail.com>
2015-05-20 23:38:58 -0700

    so map always borrows values

--- src/iterators_borrowing_and_other_stuff.rs
+++ src/iterators_borrowing_and_other_stuff.rs
@@ -5,8 +5,6 @@
 extern crate type_printer;
 
 pub fn land_of_confusion() {
-    println!("this is the land of confusion!");
-
     // first lets make a vec
     let numbas: Vec<i32> = vec![1, 2, 3, 4];
 
@@ -16,18 +14,11 @@ pub fn land_of_confusion() {
 
     // Now I can't print this
     // println!("bigger numbas: {:?}", bigger_numbas);
-    println!("bigger numbas: {:?}", bigger_numbas.collect::<Vec<i32>>());
+    // println!("bigger numbas: {:?}", bigger_numbas.collect::<Vec<i32>>());
 
     // so what happened here?
 
-    let bigger_numbas = numbas.iter().map(
-      |i|
-      {
-        type_printer::print_type_of(&i);
-        i + 1
-      }
-    );
-
+    let bigger_numbas = numbas.iter().map(|i| i + 1);
     bigger_numbas.collect::<Vec<i32>>();
 
     vec![1, 2, 3].iter().map(|i| i + 1);
@@ -40,4 +31,27 @@ pub fn land_of_confusion() {
     // each value is giving a static lifetime value
 
     // if we borrow, then it has a normal lifetime
+
+    // so I need to learn more about lifetimes!
+
+    // alright lets try and kick it up a notch all iterate and call some methods
+
+    vec![1, 2, 3].iter().map(|i| {
+        // won't compile
+        //     expected `i32`,
+        //     found `&_`
+        // numba_increaser_1(i);
+
+
+        // so i is borrowed value
+        numba_increaser_2(i);
+    });
+}
+
+fn numba_increaser_1(num: i32) -> i32 {
+    5
+}
+
+fn numba_increaser_2(num: &i32) -> i32 {
+    5
 }
