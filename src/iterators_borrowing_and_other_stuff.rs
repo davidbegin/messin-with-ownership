806021c31d60f92f52070ca441d78426c7368e9e
David Begin <davidmichaelbe@gmail.com>
2015-05-20 23:00:15 -0700

    time to figure out this iterator borrowing thang

---
+++ src/iterators_borrowing_and_other_stuff.rs
@@ -0,0 +1,7 @@
+// I find myself struggling with
+// how, when, why to use borrowed or other
+// ownership patterns when using iterators
+
+pub fn land_of_confusion() {
+    println!("this is the land of confusion!");
+}
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@
 mod ui;
 mod basic;
 mod learn_rust_ownership;
+mod iterators_borrowing_and_other_stuff;
 
 fn main() {
     ui::title();
     // basic::sample_1();
-    learn_rust_ownership::the_hard_way()
+    // learn_rust_ownership::the_hard_way()
+    iterators_borrowing_and_other_stuff::land_of_confusion();
 }
