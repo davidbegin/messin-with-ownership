eab039bb7eaa77b114e86f15439c9b6e93919c9c
David Begin <davidmichaelbe@gmail.com>
2015-05-21 00:18:43 -0700

    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit
    explict is better than implicit

--- src/iterators_borrowing_and_other_stuff.rs
+++ src/iterators_borrowing_and_other_stuff.rs
@@ -5,18 +5,29 @@
 extern crate type_printer;
 
 pub fn land_of_confusion() {
-    // initial_exploration_of_iterators_and_borrowing();
-    // iteratoring_with_methods_example_1();
-    // iteratoring_with_methods_example_2();
-    // iteratoring_with_methods_example_3();
-    for_in_and_borrowing();
+    initial_exploration_of_iterators_and_borrowing();
+    iteratoring_with_methods_example_1();
+    iteratoring_with_methods_example_2();
+    iteratoring_with_methods_example_3();
+    for_in_and_borrowing_1();
+    for_in_and_borrowing_2();
 }
 
-fn just_a_guy_trying_to_figure_out_a_type_through_compile_errors(num: &str) {
+fn for_in_and_borrowing_2() {
 
+    // hmmm this works fine, and the type is just i32
+    // but thats not borrowed?
+    for thang in vec![1, 2, 3] {
+        type_printer::print_type_of(&thang);
+        numba_increaser_that_takes_a_value_to_own(thang);
+
+        // Won't Compile because it requires a borrowed value
+        // numba_increaser_that_takes_a_borrowed_value(thang);
+        // println!("for {} in a vec", thang);
+    }
 }
 
-fn for_in_and_borrowing() {
+fn for_in_and_borrowing_1() {
     for thang in &vec![1, 2, 3] {
         type_printer::print_type_of(&thang);
         // but this prints out &'static
@@ -36,19 +47,19 @@ fn iteratoring_with_methods_example_3() {
 
     temp_vec.iter().map(|i| {
         // I reached for the stars and failed!
-        // numba_increaser_3(i)
+        // numba_increaser_that_takes_a_static_reference(i)
     });
 
     vec![1, 2, 3].iter().map(|i| {
         // another swing and a miss!
         // let num: &'static i32 = 5;
-        // numba_increaser_3(num)
+        // numba_increaser_that_takes_a_static_reference(num)
     });
 }
 
 fn iteratoring_with_methods_example_2() {
     let result = vec![1, 2, 3].iter().map(|&i| {
-        numba_increaser_1(i)
+        numba_increaser_that_takes_a_value_to_own(i)
     }).collect::<Vec<i32>>();
 
     println!("result: {:?}", result);
@@ -59,15 +70,15 @@ fn iteratoring_with_methods_example_1() {
         // won't compile
         //     expected `i32`,
         //     found `&_`
-        // numba_increaser_1(i);
+        // numba_increaser_that_takes_a_value_to_own(i);
 
 
         // so i is borrowed value
-        numba_increaser_2(i);
+        numba_increaser_that_takes_a_borrowed_value(i);
     });
 
     let result = vec![1, 2, 3].iter().map(|i| {
-        numba_increaser_2(i)
+        numba_increaser_that_takes_a_borrowed_value(i)
     }).collect::<Vec<i32>>();
 
     println!("result: {:?}", result);
@@ -108,15 +119,18 @@ fn initial_exploration_of_iterators_and_borrowing() {
     // alright lets try and kick it up a notch all iterate and call some methods
 }
 
-fn numba_increaser_1(num: i32) -> i32 {
+fn numba_increaser_that_takes_a_value_to_own(num: i32) -> i32 {
     num + 1
 }
 
-fn numba_increaser_2(num: &i32) -> i32 {
+fn numba_increaser_that_takes_a_borrowed_value(num: &i32) -> i32 {
     num + 1
 }
 
-fn numba_increaser_3(num: &'static i32) -> i32 {
+fn numba_increaser_that_takes_a_static_reference(num: &'static i32) -> i32 {
     1
+}
+
+fn just_a_guy_trying_to_figure_out_a_type_through_compile_errors(num: &str) {
 }
 
