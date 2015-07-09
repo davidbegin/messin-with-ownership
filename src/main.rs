13b80253847f6b3e3e8f641f904ca54e79c5eb74
David Begin <davidmichaelbe@gmail.com>
2015-07-08 18:18:55 -0700

    exploration of references and mutability

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
 #![feature(convert)]
 #![allow(dead_code, unused_variables, unused_must_use)]
 
+extern crate type_printer;
 mod ui;
 mod basic;
 mod learn_rust_ownership;
 mod iterators_borrowing_and_other_stuff;
+mod this_is_how_we_do_it;
 
 fn main() {
     ui::title();
     // basic::sample_1();
     // learn_rust_ownership::the_hard_way()
-    iterators_borrowing_and_other_stuff::land_of_confusion();
+    // iterators_borrowing_and_other_stuff::land_of_confusion();
+    // this_is_how_we_do_it::this_it_how_we_do_it();
+    mut_ref_what();
+}
+
+fn mut_ref_what() {
+    let mut x: i32 = 5;
+
+    // let y = &5;
+    // won't compile because although x is mutable
+    // when we take a reference it is immutable
+    // unless we say so
+    // y += 1;
+
+    // let mut z: i32 = &mut x;
+    // also won't compile
+    // expected `i32`,
+    // found `&mut i32`
+    // (expected i32,
+    // found &-ptr)
+    // z + 10;
+
+    let mut x: i32 = 5;
+    let a = x;
+    type_printer::print_type_of(&a); // i32
+
+    let mut x: i32 = 5;
+    let b = &x;
+    type_printer::print_type_of(&b); // &'static i32
+
+    let mut x: i32 = 5;
+    let c = &mut x;
+    type_printer::print_type_of(&c); // &'static mut i32
+
+    let mut x: i32 = 5;
+    let mut d = &mut x;
+    type_printer::print_type_of(&d); // &'static mut i32'
 }
---
+++ src/this_is_how_we_do_it.rs
@@ -0,0 +1,5 @@
+pub fn this_it_how_we_do_it() {
+    println!("For what feels like the 100th time
+             I am going to review ownership, borrowing, lifestimes etc.");
+    println!("=======================================================================");
+}
